ef8e2984e91e53a754e27f8967275dc99e6107f6
Bastient6 <114858037+Bastient6@users.noreply.github.com>
2023-11-28 14:51:18 +0100

    Create SendEmail.aspx.cs

---
+++ SendEmail.aspx.cs
@@ -0,0 +1,14 @@
+<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SendEmail.aspx.cs" Inherits="SendEmail" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Réponse d'envoi</title>
+    <!-- Ajoutez vos liens CSS et autres balises head ici -->
+</head>
+<body>
+    <div>
+        <% SendEmail(); %>
+    </div>
+</body>
+</html>
